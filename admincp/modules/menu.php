66e26ff56c6a9f31bae84214bedef047360e219d
hoangtb17 <phamhoang16112001@gmail.com>
2022-06-02 21:12:53 +0700

    update

---
+++ admincp/modules/menu.php
@@ -0,0 +1,8 @@
+<ul class="admincp_list">
+	<li><a href="index.php?action=quanlydanhmucsanpham&query=lietke">Quản lý danh mục sản phẩm</a></li>
+	<li><a href="index.php?action=quanlysp&query=lietke">Quản lý sản phẩm</a></li>
+	<li><a href="index.php?action=quanlykh&query=lietke">Quản lý Khách hàng</a></li>
+	<li><a href="index.php?action=quanlynhanvien&query=lietke">Quản lý nhân viên</a></li>
+	<li><a href="index.php?action=quanlydonhang&query=lietke">Quản lý đơn hàng</a></li>
+
+</ul>
